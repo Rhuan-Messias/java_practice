71b92840544040c84b5844276da8810395be3d48
Rhuan-Messias <messias.ifg@gmail.com>
2023-11-06 23:13:17 -0300

    commit

--- primitive_types.java
+++ primitive_types.java
@@ -1,5 +1,12 @@
 
-
+public class primitive_types{
+    public static void main(String[] args){
+        int anyInt = 10;
+        double anyDouble = 10;
+        double otherInt = anyInt + anyDouble;
+        System.out.println(otherInt);
+    }
+}
 class myPrimitive {
     public static void main(String args[]){
         System.out.println("primite types");
